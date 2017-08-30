cf30468d02edde2c7410ff931e2152791ecd7727
umairabbas <umairabbas@hotmail.com>
2017-08-30 13:15:13 +0200

    -fixes

--- app/src/main/java/com/dealspok/dealspok/SplashActivity.java
+++ app/src/main/java/com/dealspok/dealspok/SplashActivity.java
@@ -5,6 +5,7 @@ package com.dealspok.dealspok;
  */
 
 import android.content.Intent;
+import android.location.LocationManager;
 import android.os.Handler;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -15,6 +16,7 @@ import android.view.WindowManager;
 public class SplashActivity extends AppCompatActivity {
     private static final String TAG = SplashActivity.class.getSimpleName();
     private final int SPLASH_DISPLAY_LENGTH = 2000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -25,6 +27,10 @@ public class SplashActivity extends AppCompatActivity {
         if(null != actionBar){
             actionBar.hide();
         }
+
+        // Acquire a reference to the system Location Manager
+        LocationManager locationManager = (LocationManager) this.getSystemService(this.LOCATION_SERVICE);
+
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run(){
