99e8fea3b4ca8d65ee499ff31c868fdac825242b
Hayat\Umi <umairabbas@hotmail.com>
2017-12-28 13:56:42 +0100

    gui changes v1

--- app/src/main/java/com/dealspok/dealspok/MainActivity.java
+++ app/src/main/java/com/dealspok/dealspok/MainActivity.java
@@ -229,7 +229,7 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.search) {
             return true;
         }
 
--- app/src/main/java/com/dealspok/dealspok/Utils/JSONParser.java
+++ app/src/main/java/com/dealspok/dealspok/Utils/JSONParser.java
@@ -105,7 +105,7 @@ public class JSONParser {
 
         try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(
-                    is, "iso-8859-1"), 8);
+                    is, "UTF-8"), 8);
             StringBuilder sb = new StringBuilder();
             String line = null;
             while ((line = reader.readLine()) != null) {
