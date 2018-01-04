c75894ab2707fb7309ff9ddf7cb78c4cd578b562
Hayat\Umi <umairabbas@hotmail.com>
2018-01-04 22:09:52 +0100

    subscription new

--- app/src/main/java/com/dealspok/dealspok/SubscribeActivity.java
+++ app/src/main/java/com/dealspok/dealspok/SubscribeActivity.java
@@ -3,6 +3,7 @@ package com.dealspok.dealspok;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
@@ -13,18 +14,27 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.braintreepayments.api.dropin.DropInActivity;
 import com.braintreepayments.api.dropin.DropInRequest;
 import com.braintreepayments.api.dropin.DropInResult;
 import com.dealspok.dealspok.adapter.GutscheineAdapter;
+import com.dealspok.dealspok.entities.DealObject;
+import com.dealspok.dealspok.entities.Plans;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.RequestParams;
 import com.loopj.android.http.TextHttpResponseHandler;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import cz.msebera.android.httpclient.Header;
 
@@ -39,14 +49,23 @@ public class SubscribeActivity extends AppCompatActivity {
     private Activity activity;
     private View v;
     private static final String TAG = MainActivity.class.getSimpleName();
-    private static final String PATH_TO_SERVER = "https://regionaldeals.de/mobile/api/payment/client_token";
-    private static final String PATH_TO_SERVER_CHECKOUT = "https://regionaldeals.de/mobile/api/payment/checkout";
+    private static final String PATH_TO_SERVER = "https://regionaldeals.de/mobile/api/subscriptions/client_token";
+    private static final String PATH_TO_SERVER_SUB_DEALS = "https://www.regionaldeals.de/mobile/api/subscriptions/plans";
+    private static final String PATH_TO_SERVER_CHECKOUT = "https://regionaldeals.de/mobile/api/subscriptions/customer_vault";
     private String clientToken;
     private static final int BRAINTREE_REQUEST_CODE = 4949;
     private String paymentNonce = "";
     private String msg = "";
 //    EditText e1;
 //    EditText e2;
+    private Button firstDealBtn;
+    private TextView firstDealTv;
+    private Button secDealBtn;
+    private TextView secDealTv;
+    private JSONArray planArr = null;
+    private List<Plans> deals;
+    private String planShortName;
+    private String userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,34 +75,51 @@ public class SubscribeActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        context = this;
+        activity = this;
 
+        firstDealBtn = (Button)findViewById(R.id.btn_1);
+        secDealBtn = (Button)findViewById(R.id.btn_2);
+        firstDealTv = (TextView)findViewById(R.id.tv1);
+        secDealTv = (TextView)findViewById(R.id.tv2);
+
+        deals = new ArrayList<>();
 //        e1 = (EditText)findViewById(R.id.input_t1);
 //        e2 = (EditText)findViewById(R.id.input_t2);
 
 //        e1.setText("https://regionaldeals.de/mobile/api/payment/client_token");
 //        e2.setText("https://regionaldeals.de/mobile/api/payment/checkout");
-
-        context = this;
-        activity = this;
-
+        getSubDataFromServer();
         getClientTokenFromServer();
-        Button buyNowButton = (Button)findViewById(R.id.btn_1);
-        buyNowButton.setOnClickListener(new View.OnClickListener() {
+        firstDealBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 v = view;
+                planShortName  = deals.get(0).getPlanShortName();
                 onBraintreeSubmit(view);
             }
         });
 
-        Button buyNowButton2 = (Button)findViewById(R.id.btn_2);
-        buyNowButton2.setOnClickListener(new View.OnClickListener() {
+        secDealBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 v = view;
+                planShortName  = deals.get(1).getPlanShortName();
                 onBraintreeSubmit(view);
             }
         });
+
+        SharedPreferences prefs = getSharedPreferences(getString(R.string.sharedPredName), MODE_PRIVATE);
+        String restoredUser = prefs.getString("userObject", null);
+        try {
+            if (restoredUser != null) {
+                JSONObject obj = new JSONObject(restoredUser);
+                userId = obj.getString("userId");
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (Throwable t) {
+        }
 
     }
 
@@ -95,6 +131,49 @@ public class SubscribeActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void getSubDataFromServer(){
+        AsyncHttpClient androidClient = new AsyncHttpClient();
+        androidClient.get(PATH_TO_SERVER_SUB_DEALS, new TextHttpResponseHandler() {
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                Log.d("TAG", getString(R.string.token_failed) + responseString);
+            }
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, String responseToken) {
+                Log.d("TAG", "Client token: " + responseToken);
+                try {
+                    JSONObject obj = new JSONObject(responseToken);
+                    String dealsObj = obj.getString("data");
+                    deals.clear();
+                    planArr = new JSONArray(dealsObj);
+                    if (planArr != null) {
+                        for (int i = 0; i < planArr.length(); i++) {
+                            JSONObject c = planArr.getJSONObject(i);
+                            Gson gson = new GsonBuilder().create();
+                            Plans newDeal = gson.fromJson(c.toString(), Plans.class);
+                            deals.add(newDeal);
+                        }
+                        activity.runOnUiThread(new Runnable() {
+                            public void run() {
+                                //TODO: make dynamic
+                                firstDealBtn.setText(deals.get(0).getPlanName());
+                                firstDealTv.setText(deals.get(0).getPlanDescription());
+                                secDealBtn.setText(deals.get(1).getPlanName());
+                                secDealTv.setText(deals.get(1).getPlanDescription());
+                                //Toast.makeText(context, "Success!\n" + msg, Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    } else {
+                        Log.d("Plans: ", "null");
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                } catch (Throwable t) {
+                }
+            }
+        });
     }
 
     private void getClientTokenFromServer(){
@@ -119,6 +198,29 @@ public class SubscribeActivity extends AppCompatActivity {
         });
     }
 
+//    private void getCheckoutFromServer(){
+//        AsyncHttpClient androidClient = new AsyncHttpClient();
+//        androidClient.get(PATH_TO_SERVER_CHECKOUT+"?userid="+ userId + "&payment_method_nonce=" + paymentNonce +
+//                "&billing_plan=" + planShortName, new TextHttpResponseHandler() {
+//            @Override
+//            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+//                Log.d("TAG", getString(R.string.token_failed) + responseString);
+//            }
+//            @Override
+//            public void onSuccess(int statusCode, Header[] headers, String responseToken) {
+//                Log.d("TAG", "Client token: " + responseToken);
+//                try {
+//                    JSONObject obj = new JSONObject(responseToken);
+//                    String token = obj.getString("data");
+//                    clientToken = token;
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                } catch (Throwable t) {
+//                }
+//            }
+//        });
+//    }
+
     class checkout extends AsyncTask<String, String, String> {
 
         /**
@@ -135,7 +237,8 @@ public class SubscribeActivity extends AppCompatActivity {
                 com.dealspok.dealspok.Utils.HttpClient client = new com.dealspok.dealspok.Utils.HttpClient(PATH_TO_SERVER_CHECKOUT);
                 client.connectForMultipart();
                 client.addFormPart("payment_method_nonce", paymentNonce);
-                //client.addFormPart("userid", userId);
+                client.addFormPart("userid", userId);
+                client.addFormPart("billing_plan", planShortName);
                 client.finishMultipart();
                 resultData = client.getResponse();
                 resultData.toString();
@@ -152,6 +255,8 @@ public class SubscribeActivity extends AppCompatActivity {
             activity.runOnUiThread(new Runnable() {
                 public void run() {
                     if(msg.equals("PAYMENT_CLIENT_CHECKOUT_SUCCESS")) {
+                        Toast.makeText(context, "Success!\n" + msg, Toast.LENGTH_SHORT).show();
+                    } else if(msg.equals("PLANS_SUBSCRIPTIONS_UPDATE_OK")){
                         Toast.makeText(context, "Success!\n" + msg, Toast.LENGTH_SHORT).show();
                     }
                 }
---
+++ app/src/main/java/com/dealspok/dealspok/entities/Plans.java
@@ -0,0 +1,99 @@
+package com.dealspok.dealspok.entities;
+
+import java.io.Serializable;
+
+/**
+ * Created by Umi on 03.01.2018.
+ */
+
+public class Plans implements Serializable {
+    private Integer id;
+
+    private String planName;
+
+    private String planShortName;
+
+    private String planDescription;
+
+    private Double planPrice;
+
+    private Integer billingCycle;
+
+    private Integer numberBillingCycles;
+
+    private String currency;
+
+    private String planOffer;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPlanName() {
+        return planName;
+    }
+
+    public void setPlanName(String planName) {
+        this.planName = planName;
+    }
+
+    public String getPlanShortName() {
+        return planShortName;
+    }
+
+    public void setPlanShortName(String planShortName) {
+        this.planShortName = planShortName;
+    }
+
+    public String getPlanDescription() {
+        return planDescription;
+    }
+
+    public void setPlanDescription(String planDescription) {
+        this.planDescription = planDescription;
+    }
+
+    public Double getPlanPrice() {
+        return planPrice;
+    }
+
+    public void setPlanPrice(Double planPrice) {
+        this.planPrice = planPrice;
+    }
+
+    public Integer getBillingCycle() {
+        return billingCycle;
+    }
+
+    public void setBillingCycle(Integer billingCycle) {
+        this.billingCycle = billingCycle;
+    }
+
+    public Integer getNumberBillingCycles() {
+        return numberBillingCycles;
+    }
+
+    public void setNumberBillingCycles(Integer numberBillingCycles) {
+        this.numberBillingCycles = numberBillingCycles;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getPlanOffer() {
+        return planOffer;
+    }
+
+    public void setPlanOffer(String planOffer) {
+        this.planOffer = planOffer;
+    }
+}
